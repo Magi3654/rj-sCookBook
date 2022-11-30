4d99b1b78d83d62b76ab902b3d7774f7f0e7b641
Magi3654 <112009244+Magi3654@users.noreply.github.com>
2022-11-30 01:48:58 -0800

    tercer intento

--- app.js
+++ app.js
@@ -10,14 +10,32 @@ async function showRecipe(){
         console.log(recipe.title);
         
         const recipeName=document.createElement('p');
+        recipeName.style.fontWeight="italic";
+        recipeName.style.fontSize="20px";
         recipeName.texcontent=recipe.title;
+
         const imgRecipe=document.createElement('img');
         imgRecipe.src=recipe.image_url;
         imgRecipe.alt=recipe.name;
         imgRecipe.style.widows="95px";
 
+        const publisher = document.createElement('p');
+        publisher.style.fontSize = "15px";
+        publisher.textContent = "Publisher: "+receta.publisher;
+
+        const id = document.createElement('p');
+        id.style.fontSize = "15px";
+        id.textContent = "Id: "+receta.id;
+
         recipes.append(imgRecipe);
-        recetas.append(recipeName);
+        
+        const infoSection=document.createElement("div");
+        infoSection.appendChild(recipeName);
+        infoSection.appendChild(publisher);
+        infoSection.appendChild(id);
+        
+        recipes.append(infoSection);
+
     })
 }
 
@@ -27,18 +45,23 @@ async function comeRecipes(data){
 
 async function cleanData(){
     search.value="";
-    recipes.remove();
+    document.getElementById('recipes').innerHTML="";
 }
 
 const cookingBook= document.getElementById('cookingBook');
+
 const buscar= document.createElement('input');
 buscar.id= "search";
 buscar.style.width= "185px";
+buscar.placeholder="example: Tomato...";
+
 const boton= document.createElement('button');
 boton.textContent= "RJ's Search";
 boton.onclick= showRecipe;
+
 const clean= document.createElement('button');
 clean.textContent= "Clean";
+clean.id="Clean"
 clean.onclick= cleanData;
 
 cookingBook.append(buscar);
